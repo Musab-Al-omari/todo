ba7780a34ebc44a22ddc823682d1c3794cbaea42
Musab-Al-omari <musabsomari@gmail.com>
2021-06-21 13:51:02 +0300

    omar

--- src/components/todo/list.js
+++ src/components/todo/list.js
@@ -3,7 +3,8 @@ import React from 'react';
 import Modal from 'react-bootstrap/Modal'
 import Button from 'react-bootstrap/Button'
 import Form from 'react-bootstrap/Form'
-import { useState, useEffect } from 'react';
+
+import { useState } from 'react';
 
 
 
@@ -11,33 +12,34 @@ import { useState, useEffect } from 'react';
 function TodoList(props) {
 
   const [show, setShow] = useState(false);
-
+  let [item, setItem] = useState({});
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
 
-  let [item, setItem] = useState({});
 
+  const handleEdit = (oneItem) => {
+    handleShow();
+    setItem(oneItem)
+  }
 
 
   const handleInputChange = e => {
     setItem({ ...item, [e.target.name]: e.target.value })
   };
+  // console.log(item);
 
   const handlerPopSubmit = (e) => {
     e.preventDefault();
     e.target.reset();
-
+    handleClose();
+    // console.log(item);
     props.handlerPopSubmit(item);
-    setItem(item);
+    // setItem(item);
   };
 
 
   return (
-
-
-
-
     <>
       <ul>
         {props.list.map(item => (
@@ -51,7 +53,9 @@ function TodoList(props) {
             {/* <button onClick={() => props.editItem(item._id)}>EDIT</button> */}
             <button onClick={() => props.delete(item._id)}>delete</button>
 
-            <Button variant="primary" onClick={handleShow}>
+            <Button variant="primary" onClick={() => {
+              handleEdit(item)
+            }}>
               edit
             </Button>
 
@@ -60,40 +64,34 @@ function TodoList(props) {
       </ul>
       <Modal show={show} onHide={handleClose} animation={false}>
         <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
+          <Modal.Title>Modal Editing</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-        {/* */}
-          <Form  onSubmit={handlerPopSubmit}>
-
+          {/* */}
+          <Form onSubmit={handlerPopSubmit}>
             <Form.Group className="mb-3" controlId="formBasicEmail">
               <Form.Label>To Do Item</Form.Label>
-              <Form.Control name="text" placeholder="Add To Do List Item" onChange={handleInputChange} />
+              <Form.Control name="text" value={item.text} placeholder="Add To Do List Item" onChange={handleInputChange} />
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formBasicEmail">
               <Form.Label>Difficulty Rating</Form.Label>
-              <Form.Control defaultValue="1" min="1" max="5" type="Range" name="difficulty"  onChange={handleInputChange} />
+              <Form.Control defaultValue={item.difficulty} min="1" max="5" type="Range" name="difficulty" onChange={handleInputChange} />
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formBasicEmail">
               <Form.Label>Assigned To</Form.Label>
-              <Form.Control type="text" name="assignee" placeholder="Assigned To" onChange={handleInputChange}  />
+              <Form.Control type="text" name="assignee" value={item.assignee} placeholder="Assigned To" onChange={handleInputChange} />
             </Form.Group>
 
-            <Button variant="primary"  type="submit" onClick={handleClose}>
+            <Button variant="primary" type="submit">
               Add Item
             </Button>
             <Button variant="secondary" onClick={handleClose}>
               Close
             </Button>
-            
           </Form>
-
         </Modal.Body>
-
-
-
       </Modal>
     </>
 
